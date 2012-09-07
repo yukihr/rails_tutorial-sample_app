0aa35a7b6cb1a4698d04b26af4cbad9f43b97965
yukihiro hara <yukihr@gmail.com>
2012-09-08 00:02:31 +0900

    middle of 9.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -13,5 +16,9 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 end
