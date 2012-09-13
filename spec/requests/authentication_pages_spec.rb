d61b795869c0ad326d8e26028563e1a7646d4333
yukihiro hara <yukihr@gmail.com>
2012-09-13 19:30:27 +0900

    Ex-9.6.8 test for friendly forwarding

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -74,6 +74,13 @@ describe "Authentication" do
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
+          describe "when signing in again" do
+            before { sign_in user }
+
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name) 
+            end
+          end
         end
       end
 
