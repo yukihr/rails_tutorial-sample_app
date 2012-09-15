aff84364aef41e303d169f56246c6d8275ab099e
yukihiro hara <yukihr@gmail.com>
2012-09-15 17:06:48 +0900

    10.2.2 Sample microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
     @user = User.new
-    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
