909170a18010f2cbb39ddeaf811d3d778292d009
yukihiro hara <yukihr@gmail.com>
2012-12-18 20:36:12 +0900

    Ex 11.5.1 Add tests for destroying relationships associated with a given user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -158,7 +158,7 @@ describe User do
     end
 
     it "should destroy associated microposts" do
-      microposts = @user.microposts
+      microposts = @user.microposts.dup
       @user.destroy
       microposts.each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
@@ -207,6 +207,14 @@ describe User do
 
       it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
+    end
+
+    it "should destroy associated relationships" do
+      relationships = @user.relationships.dup
+      @user.destroy
+      relationships.each do |relationship|
+        Relationship.find_by_id(relationship.id).should be_nil
+      end
     end
   end
 
